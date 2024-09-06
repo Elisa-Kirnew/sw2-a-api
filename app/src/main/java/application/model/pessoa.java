ff80e1c7e5b86c2937eaac46bb35fa721cddeaa1
Elisa-Kirnew <elisakirnew@gmail.com>
2024-09-06 12:25:29 +0000

    Modelo Pessoa

---
+++ app/src/main/java/application/model/pessoa.java
@@ -0,0 +1,27 @@
+package application.model;
+
+public class pessoa {
+    private long id;
+    private String Nome;
+    private int idade;
+    public long getId() {
+        return id;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }
+    public String getNome() {
+        return Nome;
+    }
+    public void setNome(String nome) {
+        Nome = nome;
+    }
+    public int getIdade() {
+        return idade;
+    }
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    
+}
